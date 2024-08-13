419141ad4f70d8d335e924339b94765c7b1e169d
doeuni <ehdms1115@naver.com>
2024-08-13 16:18:26 +0900

    ✨feat: 여행지정보조회

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 import swaggerFile from './swagger/swagger-output.json' assert { type: 'json' };
-
+import { specs } from './config/swagger.config.js';
 import dotenv from 'dotenv';
 import route from './src/routes/route.js';
 import chatRoutes from './src/routes/chatRoutes.js'; // .js 확장자 주의
+import askRoute from './src/routes/chatgpt.js'; // .js 확장자 주의
 
 
 
@@ -22,6 +23,8 @@ app.use(express.urlencoded({ extended: true }));
 // 라우터 설정
 app.use('/routes', route);
 app.use(chatRoutes);
+// 라우터 설정
+app.use('/', askRoute);
 
 // Swagger 문서 설정
 app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerFile))
@@ -30,6 +33,10 @@ app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 // app.get('/routes', (req, res)=>{
 //     res.send('작동 중');
 //     })
+// 기본 라우트
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
 
 // 서버 시작
 app.listen(port, () => {
---
+++ src/dtos/detail.dto.js
@@ -0,0 +1,13 @@
+class DetailDTO {
+    constructor({
+        region,
+        district,
+        features,
+    }) {
+        this.region = region;
+        this.district = district;
+        this.features = features;
+    }
+}
+
+export default DetailDTO;
