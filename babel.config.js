f6d42cdc2fe7abf6cd85c89f17474303aeeacd04
starday119 <jy8294@naver.com>
2024-08-18 02:29:15 +0900

    👷 chore: CI/CD 배포

---
+++ babel.config.js
@@ -0,0 +1,16 @@
+// babel.config.js
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+// 현재 디렉토리의 경로를 가져옵니다.
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
+  presets: [
+    '@babel/preset-env', // 최신 JavaScript 기능을 지원하는 프리셋
+  ],
+  plugins: [
+    '@babel/plugin-syntax-import-attributes', // 'import ... assert { type: 'json' }' 구문 지원
+  ],
+};
--- webpack.config.cjs
+++ webpack.config.cjs
@@ -3,31 +3,37 @@ const path = require("path");
 
 module.exports = {
     mode: "development",
-    context: __dirname + '/src',
+
+    // context를 프로젝트 루트로 설정
+    context: path.resolve(__dirname),
+
     entry: {
-        app: '../index.js',
+        app: './index.js', // entry 파일을 src의 상위 폴더에 있는 app.js로 설정
     },
+
     output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "main.js",
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js', // output 파일의 이름을 설정
     },
+
     module: {
         rules: [
-        {
-            test: /\.js$/,
-            use: {
-            loader: "babel-loader",
-            options: {
-                presets: ["@babel/preset-env"],
+            {
+                test: /\.js$/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"],
+                    },
+                },
+                exclude: /node_modules/,
             },
-            },
-            exclude: /node_modules/,
-        },
         ],
     },
+
     target: "node",
     externalsPresets: {
         node: true,
     },
     externals: [nodeExternals()],
-    };
+};
